3e435b21a2012c3e448ec0c9e17283429abeb970
sprayxer <89803128394@mail.ru>
2023-03-10 07:35:36 +0300

    redaction

---
+++ js/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    $('.header-burger').click(function(event) {
+        $('.header-burger,.header-burger-box,.button-door').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
+
+    $('.goods-action').slick({
+        arrows:true,
+        dots:true,
+        adaptiveHeight:true,
+        slidesToShow:3,
+        slidesToScroll:1,
+        speed:500,
+        easing:'ease',
+        infinite:false,
+        responsive:[
+            {
+                breakpoint: 373,
+                settings: {
+                    slidesToShow:1,
+                }
+            }
+        ]
+    });
+});
+
